20ba984ef770b419fb6b04041e9fe9a8bf3160d1
Devansh Trivedi <trivedidevansh1080@gmail.com>
2025-07-06 04:24:14 +0530

    modifying index.js

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-console.log("🚧 Starting GraphQL server...");
-
 const express = require("express");
 const { ApolloServer } = require("@apollo/server");
 const { expressMiddleware } = require("@apollo/server/express4");
@@ -7,18 +5,14 @@ const { readFileSync } = require("fs");
 const cors = require("cors");
 const dotenv = require("dotenv");
 
-// Load env variables
 dotenv.config();
 
-// Import resolvers
 const matchResolver = require("./resolvers/matchResolver");
 const weatherResolver = require("./resolvers/weatherResolver");
 const newsResolver = require("./resolvers/newsResolver");
 
-// Load GraphQL schema
 const typeDefs = readFileSync("./schema.graphql", { encoding: "utf-8" });
 
-// Combine resolvers
 const resolvers = {
   Query: {
     ...matchResolver.Query,
@@ -27,19 +21,14 @@ const resolvers = {
   },
 };
 
-// Export a handler for Vercel
 module.exports = async (req, res) => {
   const app = express();
-
-  const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-  });
-
+  const server = new ApolloServer({ typeDefs, resolvers });
   await server.start();
 
   app.use(cors());
   app.use(express.json());
+
   app.use(
     "/",
     expressMiddleware(server, {
@@ -51,5 +40,5 @@ module.exports = async (req, res) => {
     })
   );
 
-  return app(req, res); // 🚨 Key: forward the request to express app
+  return app(req, res); // 💡 Important: this replaces app.listen()
 };
